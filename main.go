0864fc79d41d80153d2ca2cea693683b87b78334
DIMAS FEBRIYANTO <dimasfebriyanto@MacBook-Pro-DIMAS.local>
2023-02-15 20:45:51 +0700

    updated main.go

--- main.go
+++ main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"github.com/dimassfeb-09/sinaustudio.git/app"
+	"github.com/dimassfeb-09/sinaustudio.git/controllers"
+	"github.com/dimassfeb-09/sinaustudio.git/repository"
+	"github.com/dimassfeb-09/sinaustudio.git/services"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+func main() {
+	db := app.ConnectionDatabases()
+	defer db.Close()
+
+	route := gin.Default()
+	v1 := route.Group("/api/v.1/")
+
+	usersRepository := repository.NewUsersRepositoryImplementations()
+	usersService := services.NewUserServiceImplementation(db, usersRepository)
+	usersController := controllers.NewUsersControllerImplementation(usersService)
+
+	v1.POST("/user", usersController.InsertDataUser)
+	v1.PUT("/user/", usersController.UpdateDataUser)
+	v1.DELETE("/user/", usersController.DeleteDataUser)
+
+	err := route.Run(":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
