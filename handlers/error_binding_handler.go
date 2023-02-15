eda1e9cf5b578bd8d7107e20edaa70a7423b88fa
DIMAS FEBRIYANTO <dimasfebriyanto@MacBook-Pro-DIMAS.local>
2023-02-15 14:23:58 +0700

    updated error binding to one response only

--- handlers/error_binding_handler.go
+++ handlers/error_binding_handler.go
@@ -1,17 +1,44 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
 	"strings"
 
 	"github.com/go-playground/validator/v10"
 )
 
-func ErrorValidateHandler(err error) []string {
-	var errorList []string
-	for _, err := range err.(validator.ValidationErrors) {
-		field := strings.ToLower(err.Field())
-		tag := err.Tag()
-		errorList = append(errorList, "Field with key "+field+" are "+tag)
+func ErrorValidateHandler(err error) string {
+	var Msg string
+	fmt.Println(err)
+	switch err.(type) {
+	case validator.ValidationErrors:
+		for _, err := range err.(validator.ValidationErrors) {
+			field := strings.ToLower(err.Field())
+			tag := typeValidator(err.Tag())
+			Msg = fmt.Sprintf("Key: %s, Tag: %s", field, tag)
+			break
+		}
+	case *json.UnmarshalTypeError:
+		errUnmarshal := err.(*json.UnmarshalTypeError)
+		field := errUnmarshal.Field
+		tagType := errUnmarshal.Type
+		Msg = fmt.Sprintf("Key: %s, tipe data yang diizinkan: %s.", field, tagType)
+	default:
+		Msg = err.Error()
 	}
-	return errorList
+
+	return Msg
+
+}
+
+func typeValidator(tag string) string {
+	switch tag {
+	case "alpha":
+		return "string"
+	case "number":
+		return "integer"
+	default:
+		return tag
+	}
 }
