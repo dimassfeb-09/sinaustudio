f1962fff83f6251072e0078082c8e51cf1852b18
DIMAS FEBRIYANTO <dimasfebriyanto@MacBook-Pro-DIMAS.local>
2023-02-15 14:24:39 +0700

    adding error_code.go for variable error handler

---
+++ exception/error_code.go
@@ -0,0 +1,19 @@
+package exception
+
+// request
+var (
+	ERR_BAD_REQUEST_FIELD string = "ERR_BAD_REQUEST_FIELD"
+)
+
+// user
+var (
+	ERR_GET_DATA                   string = "ERR_GET_DATA"
+	ERR_NOT_FOUND                  string = "ERR_NOT_FOUND"
+	ERR_UPDATE_DATA                string = "ERR_UPDATE_DATA"
+	ERR_PREVIOUS_FIELD_NOT_ALLOWED string = "ERR_PREVIOUS_FIELD_NOT_ALLOWED"
+	ERR_ALREADY_USE                string = "ERR_ALREADY_USE"
+)
+
+var (
+	ERR_INTERNAL_SERVER string = "ERR_INTERNAL_SERVER"
+)
