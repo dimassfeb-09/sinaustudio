6d7885dc350bdc2cbe488768feac8a786db29e65
DIMAS FEBRIYANTO <dimasfebriyanto@172-10-56-201.lightspeed.moblal.sbcglobal.net>
2023-02-19 12:15:14 +0700

    modified users_controller.go

--- controllers/users_controller.go
+++ controllers/users_controller.go
@@ -9,6 +9,7 @@ import (
 	"github.com/dimassfeb-09/sinaustudio.git/services"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func NewUsersControllerImplementation(usersService services.UsersService) UsersController {
@@ -24,12 +25,10 @@ type UsersController interface {
 	InsertDataUser(c *gin.Context)
 	UpdateDataUser(c *gin.Context)
 	DeleteDataUser(c *gin.Context)
-	UpdateEmailUser(c *gin.Context)
-	UpdateNameUser(c *gin.Context)
-	UpdateNPMUser(c *gin.Context)
-	FindUserByUUID(c *gin.Context)
+	FindUserByID(c *gin.Context)
 	IsEmailRegistered(c *gin.Context)
 	IsNPMRegistered(c *gin.Context)
+	ChangePasswordUser(c *gin.Context)
 }
 
 func (u *UsersControllerImplementation) InsertDataUser(c *gin.Context) {
@@ -44,14 +43,14 @@ func (u *UsersControllerImplementation) InsertDataUser(c *gin.Context) {
 
 	isSuccess, errMsg := u.UsersService.InsertDataUser(c.Request.Context(), &user)
 	if errMsg != nil {
-		c.JSON(errMsg.StatusCode, errMsg)
+		c.AbortWithStatusJSON(errMsg.StatusCode, errMsg)
 		return
 	}
 
 	if isSuccess {
 		helpers.ToWebResponse(c, &response.SuccessResponse{
 			Success:    true,
-			StatusCode: 200,
+			StatusCode: http.StatusOK,
 			Msg:        "Sukses Create Data User",
 			Data:       nil,
 		})
@@ -70,14 +69,14 @@ func (u *UsersControllerImplementation) UpdateDataUser(c *gin.Context) {
 
 	isSuccess, errMsg := u.UsersService.UpdateDataUser(c.Request.Context(), &user)
 	if errMsg != nil {
-		c.JSON(http.StatusBadRequest, errMsg)
+		c.AbortWithStatusJSON(errMsg.StatusCode, errMsg)
 		return
 	}
 
 	if isSuccess {
 		helpers.ToWebResponse(c, &response.SuccessResponse{
 			Success:    true,
-			StatusCode: 200,
+			StatusCode: http.StatusOK,
 			Msg:        "Sukses Update Data User",
 			Data:       nil,
 		})
@@ -85,8 +84,16 @@ func (u *UsersControllerImplementation) UpdateDataUser(c *gin.Context) {
 }
 
 func (u *UsersControllerImplementation) DeleteDataUser(c *gin.Context) {
+
+	ID, err := strconv.Atoi(c.Query("id"))
+	if err != nil {
+		errMsg := helpers.ToErrorMsg(http.StatusBadRequest, "ERR_BAD_REQUEST_FIELD", "Invalid ID, fill with number ID")
+		c.JSON(http.StatusBadRequest, errMsg)
+		return
+	}
+
 	var user requests.UserDeleteRequest
-	err := c.ShouldBind(&user)
+	err = c.ShouldBind(&user)
 	if err != nil {
 		errorList := handlers.ErrorValidateHandler(err)
 		errMsg := helpers.ToErrorMsg(http.StatusBadRequest, "ERR_BAD_REQUEST_FIELD", errorList)
@@ -94,80 +101,23 @@ func (u *UsersControllerImplementation) DeleteDataUser(c *gin.Context) {
 		return
 	}
 
-	isSuccess, errMsg := u.UsersService.DeleteDataUser(c.Request.Context(), user.UUID)
+	isSuccess, errMsg := u.UsersService.DeleteDataUser(c.Request.Context(), user.ConfirmPassword, ID)
 	if errMsg != nil {
-		c.JSON(errMsg.StatusCode, errMsg)
+		c.AbortWithStatusJSON(errMsg.StatusCode, errMsg)
 		return
 	}
 
 	if isSuccess {
 		helpers.ToWebResponse(c, &response.SuccessResponse{
 			Success:    true,
-			StatusCode: 200,
+			StatusCode: http.StatusOK,
 			Msg:        "Sukses Delete Data User",
 			Data:       nil,
 		})
 	}
 }
 
-func (u *UsersControllerImplementation) UpdateEmailUser(c *gin.Context) {
-	var user requests.UserUpdateEmailRequest
-	err := c.ShouldBind(&user)
-	if err != nil {
-		errorList := handlers.ErrorValidateHandler(err)
-		errMsg := helpers.ToErrorMsg(http.StatusBadRequest, "ERR_BAD_REQUEST_FIELD", errorList)
-		c.JSON(http.StatusBadRequest, errMsg)
-		return
-	}
-
-	isSuccess, errMsg := u.UsersService.UpdateEmailUser(c.Request.Context(), user.Email, user.UUID)
-	if errMsg != nil {
-		c.JSON(http.StatusBadRequest, errMsg)
-		return
-	}
-
-	if isSuccess {
-		helpers.ToWebResponse(c, &response.SuccessResponse{
-			Success:    true,
-			StatusCode: 200,
-			Msg:        "Sukses Update Email User",
-			Data:       nil,
-		})
-	}
-}
-
-func (u *UsersControllerImplementation) UpdateNameUser(c *gin.Context) {
-	var user requests.UserUpdateNameRequest
-	err := c.ShouldBind(&user)
-	if err != nil {
-		errorList := handlers.ErrorValidateHandler(err)
-		errMsg := helpers.ToErrorMsg(http.StatusBadRequest, "ERR_BAD_REQUEST_FIELD", errorList)
-		c.JSON(http.StatusBadRequest, errMsg)
-		return
-	}
-
-	isSuccess, errMsg := u.UsersService.UpdateNameUser(c.Request.Context(), user.Name, user.UUID)
-	if errMsg != nil {
-		c.JSON(http.StatusBadRequest, errMsg)
-		return
-	}
-
-	if isSuccess {
-		helpers.ToWebResponse(c, &response.SuccessResponse{
-			Success:    true,
-			StatusCode: 200,
-			Msg:        "Sukses Update Name User",
-			Data:       nil,
-		})
-	}
-}
-
-func (u *UsersControllerImplementation) UpdateNPMUser(c *gin.Context) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (u *UsersControllerImplementation) FindUserByUUID(c *gin.Context) {
+func (u *UsersControllerImplementation) FindUserByID(c *gin.Context) {
 	//TODO implement me
 	panic("implement me")
 }
@@ -180,4 +130,40 @@ func (u *UsersControllerImplementation) IsEmailRegistered(c *gin.Context) {
 func (u *UsersControllerImplementation) IsNPMRegistered(c *gin.Context) {
 	//TODO implement me
 	panic("implement me")
+}
+
+func (u *UsersControllerImplementation) ChangePasswordUser(c *gin.Context) {
+
+	ID, err := strconv.Atoi(c.Query("id"))
+	if err != nil {
+		errMsg := helpers.ToErrorMsg(http.StatusBadRequest, "ERR_BAD_REQUEST_FIELD", "Invalid ID, fill with number ID")
+		c.JSON(http.StatusBadRequest, errMsg)
+		return
+	}
+
+	var user requests.UserChangePassword
+	err = c.ShouldBind(&user)
+	if err != nil {
+		errorList := handlers.ErrorValidateHandler(err)
+		errMsg := helpers.ToErrorMsg(http.StatusBadRequest, "ERR_BAD_REQUEST_FIELD", errorList)
+		c.JSON(http.StatusBadRequest, errMsg)
+		return
+	}
+
+	isSuccess, errMsg := u.UsersService.ChangePasswordUser(c.Request.Context(), ID, user.RecentPassword, user.NewPassword)
+	if !isSuccess && errMsg != nil {
+		c.AbortWithStatusJSON(errMsg.StatusCode, errMsg)
+
+		return
+	}
+
+	if isSuccess {
+		helpers.ToWebResponse(c, &response.SuccessResponse{
+			Success:    true,
+			StatusCode: http.StatusOK,
+			Msg:        "Sukses Change Password Data User",
+			Data:       nil,
+		})
+	}
+
 }
