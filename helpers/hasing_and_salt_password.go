5df2e4c0c325d15e2365001e636efbc6cca2d221
DIMAS FEBRIYANTO <dimasfebriyanto@172-10-56-201.lightspeed.moblal.sbcglobal.net>
2023-02-19 13:10:55 +0700

    hashing and salt password

--- helpers/hasing_and_salt_password.go
+++ helpers/hasing_and_salt_password.go
@@ -1,1 +1,13 @@
 package helpers
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashAndSaltPassword(password []byte) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
