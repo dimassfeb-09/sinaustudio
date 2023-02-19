85fd0b0ea5cada43446bdffc7bb7b6d36f466d79
DIMAS FEBRIYANTO <dimasfebriyanto@172-10-56-201.lightspeed.moblal.sbcglobal.net>
2023-02-19 13:09:29 +0700

    modified microservice

--- app/microservices.go
+++ app/microservices.go
@@ -1,1 +1,39 @@
 package app
+
+import (
+	"github.com/dimassfeb-09/sinaustudio.git/repository"
+)
+
+type MicroServiceServer interface {
+	UserRepository() repository.UsersRepository
+	AuthRepository() repository.AuthRepository
+	ClassRepository() repository.ClassRepository
+	LectureRepository() repository.LectureRepository
+}
+
+type MicroService struct {
+	User    repository.UsersRepository
+	Auth    repository.AuthRepository
+	Class   repository.ClassRepository
+	Lecture repository.LectureRepository
+}
+
+func NewMicroService(usersRepository repository.UsersRepository, authRepository repository.AuthRepository, classRepository repository.ClassRepository, lectureRepository repository.LectureRepository) MicroServiceServer {
+	return &MicroService{User: usersRepository, Auth: authRepository, Class: classRepository, Lecture: lectureRepository}
+}
+
+func (m *MicroService) UserRepository() repository.UsersRepository {
+	return m.User
+}
+
+func (m *MicroService) AuthRepository() repository.AuthRepository {
+	return m.Auth
+}
+
+func (m *MicroService) ClassRepository() repository.ClassRepository {
+	return m.Class
+}
+
+func (m *MicroService) LectureRepository() repository.LectureRepository {
+	return m.Lecture
+}
