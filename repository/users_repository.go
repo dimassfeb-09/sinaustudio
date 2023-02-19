726521733c15a32c00b0b7d7bf3eed729a3cb88b
DIMAS FEBRIYANTO <dimasfebriyanto@172-10-56-201.lightspeed.moblal.sbcglobal.net>
2023-02-19 12:11:09 +0700

    modified lecture_repository.go

--- repository/users_repository.go
+++ repository/users_repository.go
@@ -3,8 +3,6 @@ package repository
 import (
 	"context"
 	"database/sql"
-	"errors"
-	"fmt"
 	"net/http"
 
 	"github.com/dimassfeb-09/sinaustudio.git/entity/domain"
@@ -16,15 +14,14 @@ import (
 type UsersRepository interface {
 	InsertDataUser(ctx context.Context, tx *sql.Tx, user *domain.Users) (isSuccess bool, errMsg *response.ErrorMsg)
 	UpdateDataUser(ctx context.Context, tx *sql.Tx, user *domain.Users) (isSuccess bool, errMsg *response.ErrorMsg)
-	DeleteDataUser(ctx context.Context, tx *sql.Tx, UUID string) (isSuccess bool, errMsg *response.ErrorMsg)
-	UpdateEmailUser(ctx context.Context, tx *sql.Tx, recentEmail string, newEmail string) (isSuccess bool, errMsg *response.ErrorMsg)
-	UpdateNameUser(ctx context.Context, tx *sql.Tx, user *domain.Users) (isSuccess bool, errMsg *response.ErrorMsg)
-	UpdateNPMUser(ctx context.Context, tx *sql.Tx, user *domain.Users) (isSuccess bool, errMsg *response.ErrorMsg)
-	FindUserByUUID(ctx context.Context, db *sql.DB, UUID string) (userResponse *domain.Users, isRegistered bool, errMsg *response.ErrorMsg)
+	DeleteDataUser(ctx context.Context, tx *sql.Tx, ID int) (isSuccess bool, errMsg *response.ErrorMsg)
+	FindUserByID(ctx context.Context, db *sql.DB, ID int) (userResponse *domain.Users, isRegistered bool, errMsg *response.ErrorMsg)
+	FindUserByEmail(ctx context.Context, db *sql.DB, email string) (userResponse *domain.Users, errMsg *response.ErrorMsg)
 	IsEmailRegistered(ctx context.Context, db *sql.DB, email string) (userResponse *domain.Users, isRegistered bool, errMsg *response.ErrorMsg)
 	IsNPMRegistered(ctx context.Context, db *sql.DB, npm string) (userResponse *domain.Users, isRegistered bool, errMsg *response.ErrorMsg)
+	ChangePasswordUser(ctx context.Context, tx *sql.Tx, newPass string, ID int) (isSuccess bool, errMsg *response.ErrorMsg)
+	FindUserByClassID(ctx context.Context, db *sql.DB, classID int) (userResponse []domain.Users, isRegistered bool, errMsg *response.ErrorMsg)
 }
-
 type UsersRepositoryImplementations struct {
 }
 
@@ -33,8 +30,8 @@ func NewUsersRepositoryImplementations() UsersRepository {
 }
 
 func (u *UsersRepositoryImplementations) InsertDataUser(ctx context.Context, tx *sql.Tx, user *domain.Users) (isSuccess bool, errMsg *response.ErrorMsg) {
-	sqlQuery := "INSERT INTO users(uuid, name, email, class_id, npm, role) VALUES(?, ?, ?, ?, ?, ?)"
-	_, err := tx.ExecContext(ctx, sqlQuery, &user.UUID, &user.Name, &user.Email, &user.ClassID, &user.NPM, &user.Role)
+	sqlQuery := "INSERT INTO users(name, email, password, class_id, npm, role) VALUES(?, ?, ?, ?, ?, ?)"
+	_, err := tx.ExecContext(ctx, sqlQuery, &user.Name, &user.Email, &user.Password, &user.ClassID, &user.NPM, &user.Role)
 	if err != nil {
 		return false, helpers.ToErrorMsg(http.StatusInternalServerError, exception.ERR_INTERNAL_SERVER, err)
 	}
@@ -42,18 +39,17 @@ func (u *UsersRepositoryImplementations) InsertDataUser(ctx context.Context, tx 
 }
 
 func (u *UsersRepositoryImplementations) UpdateDataUser(ctx context.Context, tx *sql.Tx, user *domain.Users) (isSuccess bool, errMsg *response.ErrorMsg) {
-	sqlQuery := "UPDATE users SET name = ?, email = ?, npm = ?, role = ?, class_id = ? WHERE uuid = ?"
-	fmt.Println(user)
-	_, err := tx.ExecContext(ctx, sqlQuery, &user.Name, &user.Email, &user.NPM, &user.Role, &user.ClassID, &user.UUID)
+	sqlQuery := "UPDATE users SET name = ?, email = ?, npm = ?, role = ?, class_id = ? WHERE id = ?"
+	_, err := tx.ExecContext(ctx, sqlQuery, &user.Name, &user.Email, &user.NPM, &user.Role, &user.ClassID, &user.ID)
 	if err != nil {
 		return false, helpers.ToErrorMsg(http.StatusInternalServerError, exception.ERR_INTERNAL_SERVER, err)
 	}
 	return true, nil
 }
 
-func (u *UsersRepositoryImplementations) DeleteDataUser(ctx context.Context, tx *sql.Tx, UUID string) (isSuccess bool, errMsg *response.ErrorMsg) {
-	sqlQuery := "DELETE FROM users WHERE uuid = ?"
-	_, err := tx.ExecContext(ctx, sqlQuery, UUID)
+func (u *UsersRepositoryImplementations) DeleteDataUser(ctx context.Context, tx *sql.Tx, ID int) (isSuccess bool, errMsg *response.ErrorMsg) {
+	sqlQuery := "DELETE FROM users WHERE id = ?"
+	_, err := tx.ExecContext(ctx, sqlQuery, ID)
 	if err != nil {
 		return false, helpers.ToErrorMsg(http.StatusInternalServerError, exception.ERR_INTERNAL_SERVER, err)
 	}
@@ -69,9 +65,9 @@ func (u *UsersRepositoryImplementations) UpdateEmailUser(ctx context.Context, tx
 	return true, nil
 }
 
-func (u *UsersRepositoryImplementations) FindUserByUUID(ctx context.Context, db *sql.DB, UUID string) (userResponse *domain.Users, isRegistered bool, errMsg *response.ErrorMsg) {
-	querySql := "SELECT id, uuid, name, email, class_id FROM users WHERE uuid = ?"
-	row, err := db.QueryContext(ctx, querySql, UUID)
+func (u *UsersRepositoryImplementations) FindUserByID(ctx context.Context, db *sql.DB, ID int) (userResponse *domain.Users, isRegistered bool, errMsg *response.ErrorMsg) {
+	querySql := "SELECT id, name, email, password, class_id FROM users WHERE id = ?"
+	row, err := db.QueryContext(ctx, querySql, ID)
 	if err != nil {
 		return nil, false, helpers.ToErrorMsg(http.StatusInternalServerError, exception.ERR_INTERNAL_SERVER, err)
 	}
@@ -79,37 +75,40 @@ func (u *UsersRepositoryImplementations) FindUserByUUID(ctx context.Context, db 
 
 	var user domain.Users
 	if row.Next() {
-		err := row.Scan(&user.ID, &user.UUID, &user.Name, &user.Email, &user.ClassID)
+		err := row.Scan(&user.ID, &user.Name, &user.Email, &user.Password, &user.ClassID)
 		if err != nil {
 			return nil, false, helpers.ToErrorMsg(http.StatusInternalServerError, exception.ERR_GET_DATA, err)
 		} else {
 			return &user, true, nil
 		}
 	} else {
-		return nil, false, helpers.ToErrorMsg(http.StatusNotFound, exception.ERR_GET_DATA, errors.New("Data User UUID tidak ditemukan."))
+		return nil, false, helpers.ToErrorMsg(http.StatusNotFound, exception.ERR_NOT_FOUND, "Data User ID tidak ditemukan.")
 	}
 }
 
-func (u *UsersRepositoryImplementations) UpdateNameUser(ctx context.Context, tx *sql.Tx, user *domain.Users) (isSuccess bool, errMsg *response.ErrorMsg) {
-	querySql := "UPDATE users SET name = ? WHERE uuid = ?"
-	_, err := tx.QueryContext(ctx, querySql, &user.Name, &user.UUID)
+func (u *UsersRepositoryImplementations) FindUserByEmail(ctx context.Context, db *sql.DB, email string) (userResponse *domain.Users, errMsg *response.ErrorMsg) {
+	querySql := "SELECT id, name, email, npm, role, class_id FROM users WHERE email = ?"
+	rows, err := db.QueryContext(ctx, querySql, email)
 	if err != nil {
-		return false, helpers.ToErrorMsg(http.StatusInternalServerError, exception.ERR_INTERNAL_SERVER, err)
+		return nil, helpers.ToErrorMsg(http.StatusInternalServerError, exception.ERR_INTERNAL_SERVER, err)
 	}
-	return true, nil
-}
+	defer rows.Close()
 
-func (u *UsersRepositoryImplementations) UpdateNPMUser(ctx context.Context, tx *sql.Tx, user *domain.Users) (isSuccess bool, errMsg *response.ErrorMsg) {
-	querySql := "UPDATE users SET npm = ? WHERE uuid = ?"
-	_, err := tx.QueryContext(ctx, querySql, &user.NPM, &user.UUID)
-	if err != nil {
-		return false, helpers.ToErrorMsg(http.StatusInternalServerError, exception.ERR_INTERNAL_SERVER, err)
+	var user domain.Users
+	if rows.Next() {
+		err := rows.Scan(&user.ID, &user.Name, &user.Email, &user.NPM, &user.Role, &user.ClassID)
+		if err != nil {
+			return nil, helpers.ToErrorMsg(http.StatusInternalServerError, exception.ERR_INTERNAL_SERVER, err)
+		}
+		return &user, nil
+	} else {
+		return nil, helpers.ToErrorMsg(http.StatusNotFound, exception.ERR_NOT_FOUND, "Email tidak ditemukan.")
 	}
-	return true, nil
+
 }
 
 func (u *UsersRepositoryImplementations) IsEmailRegistered(ctx context.Context, db *sql.DB, email string) (userResponse *domain.Users, isRegistered bool, errMsg *response.ErrorMsg) {
-	querySql := "SELECT uuid, email FROM users WHERE email = ?"
+	querySql := "SELECT id, email FROM users WHERE email = ?"
 	row, err := db.QueryContext(ctx, querySql, email)
 	if err != nil {
 		return nil, false, helpers.ToErrorMsg(http.StatusInternalServerError, "ERR_INTERNAL_SERVER_ERROR", err)
@@ -118,7 +117,7 @@ func (u *UsersRepositoryImplementations) IsEmailRegistered(ctx context.Context, 
 
 	var user domain.Users
 	if row.Next() {
-		err := row.Scan(&user.UUID, &user.Email)
+		err := row.Scan(&user.ID, &user.Email)
 		if err != nil {
 			return nil, false, helpers.ToErrorMsg(http.StatusNotFound, "ERR_INTERNAL_SERVER_ERROR", err)
 		}
@@ -129,7 +128,7 @@ func (u *UsersRepositoryImplementations) IsEmailRegistered(ctx context.Context, 
 }
 
 func (u *UsersRepositoryImplementations) IsNPMRegistered(ctx context.Context, db *sql.DB, npm string) (userResponse *domain.Users, isRegistered bool, errMsg *response.ErrorMsg) {
-	querySql := "SELECT uuid, npm FROM users WHERE npm = ?"
+	querySql := "SELECT id, npm FROM users WHERE npm = ?"
 	row, err := db.QueryContext(ctx, querySql, npm)
 	if err != nil {
 		return nil, false, helpers.ToErrorMsg(http.StatusInternalServerError, "INTERNAL_SERVER_ERROR", err)
@@ -138,7 +137,7 @@ func (u *UsersRepositoryImplementations) IsNPMRegistered(ctx context.Context, db
 
 	var user domain.Users
 	if row.Next() {
-		err := row.Scan(&user.UUID, &user.NPM)
+		err := row.Scan(&user.ID, &user.NPM)
 		if err != nil {
 			return nil, false, helpers.ToErrorMsg(http.StatusNotFound, "ERR_INTERNAL_SERVER_ERROR", err)
 		}
@@ -146,4 +145,34 @@ func (u *UsersRepositoryImplementations) IsNPMRegistered(ctx context.Context, db
 	} else {
 		return nil, false, helpers.ToErrorMsg(http.StatusNotFound, "ERR_NOT_FOUND", "Data tidak ditemukan")
 	}
+}
+
+func (u *UsersRepositoryImplementations) ChangePasswordUser(ctx context.Context, tx *sql.Tx, newPass string, ID int) (isSuccess bool, errMsg *response.ErrorMsg) {
+	querySql := "UPDATE users SET password = ? WHERE id = ?"
+	_, err := tx.ExecContext(ctx, querySql, newPass, ID)
+	if err != nil {
+		return false, helpers.ToErrorMsg(http.StatusInternalServerError, exception.ERR_INTERNAL_SERVER, err)
+	}
+	return true, nil
+}
+
+func (u *UsersRepositoryImplementations) FindUserByClassID(ctx context.Context, db *sql.DB, classID int) (userResponse []domain.Users, isRegistered bool, errMsg *response.ErrorMsg) {
+	querySql := "SELECT id, name, class_id FROM users WHERE class_id = ? LIMIT 1"
+	rows, err := db.QueryContext(ctx, querySql, classID)
+	if err != nil {
+		return nil, false, helpers.ToErrorMsg(http.StatusInternalServerError, exception.ERR_INTERNAL_SERVER, err)
+	}
+	defer rows.Close()
+
+	var users []domain.Users
+	for rows.Next() {
+		var user domain.Users
+		err := rows.Scan(&user.ID, &user.Name, &user.ClassID)
+		if err != nil {
+			return nil, false, helpers.ToErrorMsg(http.StatusInternalServerError, exception.ERR_INTERNAL_SERVER, err)
+		} else {
+			users = append(users, user)
+		}
+	}
+	return users, true, nil
 }
