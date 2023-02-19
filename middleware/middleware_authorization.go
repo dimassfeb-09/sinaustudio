d5daf08d79e66ecc9b4c3799c9dac0f03918279a
DIMAS FEBRIYANTO <dimasfebriyanto@172-10-56-201.lightspeed.moblal.sbcglobal.net>
2023-02-19 12:06:37 +0700

    Modified middleware for required authorization, access allow and jwt

--- middleware/middleware_authorization.go
+++ middleware/middleware_authorization.go
@@ -1,1 +1,27 @@
 package middleware
+
+import (
+	"github.com/dimassfeb-09/sinaustudio.git/exception"
+	"github.com/dimassfeb-09/sinaustudio.git/helpers"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
+
+func MiddlewareAuthorizationfunc(c *gin.Context) {
+	authorization := c.Request.Header.Get("Authorization")
+	if authorization == "" {
+		errMsg := helpers.ToErrorMsg(http.StatusUnauthorized, exception.ERR_UNAUTHORIZED_BEARER, "Key: Header with key Authorization: Bearer Token, Tag: Required")
+		c.AbortWithStatusJSON(errMsg.StatusCode, errMsg)
+		return
+	}
+	bearers := strings.Split(authorization, "Bearer")
+	if bearers[1] == "" {
+		errMsg := helpers.ToErrorMsg(http.StatusUnauthorized, exception.ERR_UNAUTHORIZED_BEARER, "Key: Token not found, Tag: Required")
+		c.AbortWithStatusJSON(errMsg.StatusCode, errMsg)
+		return
+	} else {
+		CheckingJWTToken(bearers[1], c)
+		c.Next()
+	}
+}
--- middleware/middleware_jwt.go
+++ middleware/middleware_jwt.go
@@ -1,44 +1,88 @@
-package helpers
+package middleware
 
 import (
+	"errors"
+	"fmt"
+	"github.com/dimassfeb-09/sinaustudio.git/exception"
+	"github.com/dimassfeb-09/sinaustudio.git/helpers"
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
+	"net/http"
+	"strings"
 	"time"
 )
 
-type UsersInfoJWT struct {
+type CustomJWTClaims struct {
+	*jwt.RegisteredClaims
 	ID      int    `json:"id"`
 	Name    string `json:"name"`
 	Email   string `json:"email"`
 	NPM     string `json:"npm"`
+	Role    string `json:"role"`
 	ClassID int    `json:"class_id"`
 }
 
-type CustomJWTClaims struct {
-	jwt.RegisteredClaims
-	UsersInfo UsersInfoJWT `json:"users_info"`
+type UserInfo struct {
+	ID      int
+	Name    string
+	Email   string
+	NPM     string
+	Role    string
+	ClassID int
 }
 
-func JWTGenereateToken(info UsersInfoJWT) (string, error) {
+var mySigningKey = []byte("7asd23&*^*($^&)**#$_hjagsd$#23496723")
 
-	expiredTime := time.Now().Add(time.Hour * 24)
+func CheckingJWTToken(tokenBearer string, c *gin.Context) {
 
-	claims := CustomJWTClaims{
-		RegisteredClaims: jwt.RegisteredClaims{
+	tokenResult := strings.TrimLeft(tokenBearer, " ")
+	token, err := jwt.Parse(tokenResult, func(token *jwt.Token) (interface{}, error) {
+		if method, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Signing method invalid")
+		} else if method != jwt.SigningMethodHS256 {
+			return nil, fmt.Errorf("Signing method invalid")
+		}
+
+		c.Header("Authorization", fmt.Sprintf("Bearer %s", token.Raw))
+		c.SetCookie("Authorization", fmt.Sprintf("Bearer %s", token.Raw), 10, "/", "localhost:8080", true, true)
+
+		return mySigningKey, nil
+	})
+
+	if err != nil {
+		if errors.Is(err, &jwt.ValidationError{}) {
+			errMsg := helpers.ToErrorMsg(http.StatusBadRequest, exception.ERR_BAD_REQUEST_FIELD, err.(*jwt.ValidationError).Error())
+			c.AbortWithStatusJSON(errMsg.StatusCode, errMsg)
+			return
+		}
+	}
+
+	_, ok := token.Claims.(jwt.MapClaims)
+	if !token.Valid || !ok {
+		errMsg := helpers.ToErrorMsg(http.StatusBadRequest, exception.ERR_BAD_REQUEST_FIELD, "Token tidak valid!")
+		c.AbortWithStatusJSON(errMsg.StatusCode, errMsg)
+		return
+	}
+}
+
+func JWTGenereateToken(info *UserInfo) (string, error) {
+
+	claims := &CustomJWTClaims{
+		RegisteredClaims: &jwt.RegisteredClaims{
 			Issuer:    "SINAUSTUDIO",
 			Subject:   "Sinau Studios is",
-			ExpiresAt: jwt.NewNumericDate(expiredTime),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)),
 			NotBefore: jwt.NewNumericDate(time.Now()),
 			IssuedAt:  jwt.NewNumericDate(time.Now()),
 		},
-		UsersInfo: UsersInfoJWT{
-			ID:      info.ID,
-			Name:    info.Name,
-			Email:   info.Email,
-			NPM:     info.NPM,
-			ClassID: info.ClassID,
-		},
+		ID:      info.ID,
+		Name:    info.Name,
+		Email:   info.Name,
+		NPM:     info.NPM,
+		Role:    info.Role,
+		ClassID: info.ClassID,
 	}
-	mySigningKey := []byte("7asd23&*^*($^&)**#$_hjagsd$#23496723")
+
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	myToken, err := token.SignedString(mySigningKey)
 	if err != nil {
